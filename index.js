3219094f46067f5d482d634908c7840b2f8247bd
tsv2013 <tsv2013@users.noreply.github.com>
2016-02-10 18:30:46 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+	ko.bindingHandlers.select2 = {
+	    after: ["options", "value"],
+	    init: function(el, valueAccessor, allBindingsAccessor, viewModel) {
+	      $(el).select2(ko.unwrap(valueAccessor()));
+	      ko.utils.domNodeDisposal.addDisposeCallback(el, function() {
+	          $(el).select2('destroy');
+	      });
+	    },
+	    update: function (el, valueAccessor, allBindingsAccessor, viewModel) {
+	        var allBindings = allBindingsAccessor();
+	        var select2 = $(el).data("select2");
+	        if("value" in allBindings) {
+	            var newValue = "" + ko.unwrap(allBindings.value);
+	            if((allBindings.select2.multiple || el.multiple) && newValue.constructor !== Array) {                
+	                select2.val(newValue.split(","));
+	            }
+	            else {
+	                select2.val(newValue);
+	            }
+	        }                    
+	    }
+	};
